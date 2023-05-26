5901e255f751d4ed6698bc977289407346d0b810
Amindhm <amindhm1313@gmail.com>
2023-05-26 17:39:45 +0330

    Add Comment to .sh file

---
+++ KE-Python/MyProject/copy-to-my-folder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Change Directory to our KE
+cd ./KE-Python
+
+# Make Folder
+mkdir YourName
+
+cd ./YourName
+
+# Copy project
+cp -r ../MyProject/Bizz-Fizz .
+cp -r ../MyProject/Days-ofweek .
