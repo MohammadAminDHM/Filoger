ff8efec25adaffed7c6b69ffc912ce7f516ca221
Samira <samira-ai-2020>
2023-05-26 21:05:27 +0330

    i'm update this!

--- KE-Python/copy-to-my-folder.sh
+++ KE-Python/copy-to-my-folder.sh
@@ -3,10 +3,11 @@
 cd ./KE-Python
 
 # Create our folder for storing the files and directories we want zipped
-mkdir YourName
+mkdir Samira
 
-cd ./YourName
+cd ./Samira
 
 # Trailing . is important if you want to copy all files and subdirectories in "folder1"
 cp -r ../MyProject/Bizz-Fizz .
-cp -r ../MyProject/Days-ofweek .
+cp -r ../MyProject/Days-of-week .
+
