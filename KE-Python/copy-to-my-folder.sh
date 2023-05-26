e0b16304cd041f44df935b70e7140ba743803846
Simin Alavizadeh <simin.alavizadeh@gmail.com>
2023-05-26 20:03:34 +0330

    I update code

--- KE-Python/copy-to-my-folder.sh
+++ KE-Python/copy-to-my-folder.sh
@@ -6,7 +6,7 @@ cd ./KE-Python
 mkdir Simin
 
 cd ./Simin
-
+#comment
 # Trailing . is important if you want to copy all files and subdirectories in "folder1"
 cp -r ../MyProject/Bizz-Fizz .
 cp -r ../MyProject/Days-of-week .
