dfd4183c981822f9a40e842a7aa40c59466a47ff
Yasaman <yasamanoliaee@gmail.com>
2023-05-26 21:17:47 +0430

    i am yasaman

--- KE-Python/copy-to-my-folder.sh
+++ KE-Python/copy-to-my-folder.sh
@@ -3,10 +3,10 @@
 cd ./KE-Python
 
 # Create our folder for storing the files and directories we want zipped
-mkdir YourName
+mkdir Yasaman
 
-cd ./YourName
+cd ./Yasaman
 
 # Trailing . is important if you want to copy all files and subdirectories in "folder1"
 cp -r ../MyProject/Bizz-Fizz .
-cp -r ../MyProject/Days-ofweek .
+cp -r ../MyProject/Days-of-week .
