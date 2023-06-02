d4fdea0651fdcc6b8ac870651ec6a0f608ce60f0
Zeka <aminamindhm1212@gmail.com>
2023-06-02 20:42:43 +0330

    Fill Content of OOp

---
+++ KE-OOP/MAminDHM/Math/Use.py
@@ -0,0 +1,7 @@
+from Calculator import Complex,Calculator
+
+print(Calculator.__doc__)
+
+a =  Complex(5, '10+100i')
+
+print(a.add_complex())
---
+++ KE-OOP/MAminDHM/Test.py
@@ -0,0 +1,11 @@
+class person:
+    """empty
+    dsadsajdasop
+    dsadsahoidjsa
+    dsajdksa"""
+    
+    def pp(self, a):
+        print(a)
+
+a = person()
+print(a.pp(2))
